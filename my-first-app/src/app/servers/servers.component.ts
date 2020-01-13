acc03e95faeb1d3858d4c47df1211c8cc0db6556
unknown <rydzaka12@gmail.com>
2020-01-13 13:20:22 -0600

    added more components with direct styles

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
+  // template: `<app-server></app-server> <h1>Hello There</h1>
+  //            <app-server></app-server>`,
+  // styles: [`h4{color:green}`]
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
