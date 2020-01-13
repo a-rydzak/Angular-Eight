e3fa22644cee602f0cb96bce2d9ad1e3347fbd37
unknown <rydzaka12@gmail.com>
2020-01-13 13:51:01 -0600

    added Typescript Notes

---
+++ TypeScript/notes.js
@@ -0,0 +1,6 @@
+// see what I compile to with tsc .\notes.ts
+// then run node notes.js
+'use strict';
+exports.__esModule = true;
+var message = 'I am not a message...';
+console.log(message);
---
+++ TypeScript/notes.ts
@@ -0,0 +1,8 @@
+
+// see what I compile to with tsc .\notes.ts
+// then run node notes.js
+'use strict'
+const message = 'I am not a message...'
+console.log(message)
+
+export {}
